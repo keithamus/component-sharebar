d7ca46141ffe86dc8845aabf2c1120c375c81004
umbobabo <umbertobabini@economist.com>
2015-07-02 15:17:31 +0100

    Create first prototype of share link monitoring

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # component-sharebar
+
+React component that creates a sharebar for the main socials website
--- example.js
+++ example.js
@@ -2,5 +2,11 @@ import ShareBar from './index';
 import React from 'react';
 
 export default (
-  <ShareBar></ShareBar>
+  <div>
+    <ShareBar></ShareBar>
+    <ShareBar layout="vertical"></ShareBar>
+    <ShareBar useSvg={false}></ShareBar>
+    <ShareBar useSvg={false} layout="vertical"></ShareBar>
+    <script type="text/javascript" src="https://cdn.static-economist.com/sites/default/files/external/ec_omniture/3_2/ec_omniture_s_code.js"></script>
+  </div>
 );
