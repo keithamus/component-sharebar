0ff996271398d7fd6850371d595c84d59e62f029
umbobabo <umbertobabini@economist.com>
2015-07-07 17:49:19 +0100

    Linted

--- example.js
+++ example.js
@@ -1,31 +1,38 @@
 import ShareBar from './index';
 import Omniture from '@economist/component-omniture';
 import React from 'react';
-
-var pageSetting = {
+const pageSetting = {
   s: {
-    linkTrackVars: "events,pageName,prop45,eVar52,eVar53",
-    pageName: "This is the pageName"
+    linkTrackVars: 'events,pageName,prop45,eVar52,eVar53',
+    pageName: 'This is the pageName',
   },
   Omniture: {
-    position: "share-bar",
-    page: "Name of the page",
-    hostpage: "hostpage",
-    linkinfo: ["the_world_if","section","article headline"]
-  }
-}
-
+    position: 'share-bar',
+    page: 'Name of the page',
+    hostpage: 'hostpage',
+    linkinfo: [ 'the_world_if', 'section', 'article headline' ],
+  },
+};
 export default (
   <div>
     <h1>SVG version</h1>
-    <ShareBar {...pageSetting} background="#CCFFDD"></ShareBar>
+    <ShareBar {...pageSetting} background="#CCFFDD" />
     <hr/>
-    <ShareBar layout="vertical"></ShareBar>
+    <ShareBar layout="vertical" />
     <hr/>
     <Omniture />
     <h1>Flip version</h1>
-    <ShareBar useFX={true} fxDirection="flip-to-top" fxType="cube" background="#333333" fxDefaultStateBackground="#999999"></ShareBar>
+    <ShareBar useFX={true}
+    fxDirection="flip-to-top"
+    fxType="cube"
+    background="#333333"
+    fxDefaultStateBackground="#999999" />
     <hr/>
-    <ShareBar useFX={true} fxDirection="flip-to-bottom" fxType="cube" background="#333333" fxDefaultStateBackground="#999999"></ShareBar>
+    <ShareBar
+    useFX={true}
+    fxDirection="flip-to-bottom"
+    fxType="cube"
+    background="#333333"
+    fxDefaultStateBackground="#999999" />
   </div>
 );
